d276531296f6bdd970ee98a92c812480b1757e7d
Danilo Levantesi <danilo.levantesi@iongroup.com>
2023-09-27 13:02:17 +0100

    Added code to submit trades via TradeExecution API (UC1)

---
+++ src/main/java/com/ion/barclaysapi/usecases/uc1/Constants.java
@@ -0,0 +1,15 @@
+package com.ion.barclaysapi.usecases.uc1;
+
+public class Constants {
+	public static final String xParticipantId = "TEAM016";
+	public static final String xApiKey = "TEAM016_pojpjkbhbUTFUF5r7646ghwjyef3ww5uwygfuy9094UUG9_016";
+
+	
+	public static final String FINANCIAL_MEMBER_CLIENT01 = "CLIENT01";
+	public static final String FINANCIAL_MEMBER_CLIENT02 = "CLIENT02";
+	public static final String FINANCIAL_MEMBER_CLIENT03 = "CLIENT03";
+	public static final String FINANCIAL_MEMBER_DEALER01 = "DEALER01";
+	public static final String FINANCIAL_MEMBER_DEALER02 = "DEALER02";
+
+	
+}
